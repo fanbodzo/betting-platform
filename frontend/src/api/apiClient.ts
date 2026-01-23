c04fbcbb997f5f89688f34d28a06a728baa236d2
Filip <82474139+fanbodzo@users.noreply.github.com>
2026-01-23 18:00:06 +0100

    Update apiClient.ts

--- frontend/src/api/apiClient.ts
+++ frontend/src/api/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const apiClient = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
+    baseURL: "http://74.249.43.156:9000",
 });
 
 apiClient.interceptors.request.use((config) => {
